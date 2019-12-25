c490f2d2a0a403b769d8c1ec5ea6e8e001326811
余钱 <306594076@qq.com>
2019-12-25 16:09:44 +0800

    Web project creation Ref #6

---
+++ myweb/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
