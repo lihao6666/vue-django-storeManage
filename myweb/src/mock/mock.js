a9b9f49dbc0606d5d429a5fb5793347834e60043
余钱 <306594076@qq.com>
2019-12-28 17:09:00 +0800

    Update yq. Ref #6

--- myweb/src/mock/mock.js
+++ myweb/src/mock/mock.js
@@ -5,3 +5,5 @@ Mock.mock('/req_pur_prd', 'post', require('./json/req_pur_prd'))
 
 Mock.mock('/so_check', 'post', require('./json/so_check'))
 Mock.mock('/so_sod', 'post', require('./json/so_sod'))
+
+Mock.mock('/test2', 'post', require('./json/test2'))
