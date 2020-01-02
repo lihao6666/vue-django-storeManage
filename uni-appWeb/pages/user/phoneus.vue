c7cf20c9ec03f9d1e7be629a3a0d6eea8ef61a73
张恩普 <516380637@qq.com>
2020-01-02 12:40:08 +0800

    New main page Ref #20

--- uni-appWeb/pages/user/phoneus.vue
+++ uni-appWeb/pages/user/phoneus.vue
@@ -1,9 +1,20 @@
 <template>
 	<view class="content">
+		<view class="blankBox"></view>
 		<view class="aboutUs">
 			<view class="title">关于我们</view>
 			<view class="infomation">
 			    本产品由"Mars456"小组独立开发。"Mars456"小组来自合肥工业大学软件学院。如果你对互联网产品开发有兴趣，欢迎通过以下方式联系我们。
+			</view>
+		</view>
+		<view class="blankBox"></view>
+		<view class="aboutUs">
+			<view class="title">联系方式</view>
+			<view class="infomation">
+				<view>电话：13505656290</view>
+			    <view>QQ：516380637</view>
+			    <view>微信：hylh818</view>
+				<view>微博：Mars456</view>
 			</view>
 		</view>
 	</view>
@@ -16,13 +27,19 @@
 	.content {
 		padding: 0;
 	}
+	.blankBox {
+		height: 40upx;
+	}
 	.aboutUs {
 		background: #FFFFFF;
 	}
 	.title {
 		border-top: 10upx;
+		font-size: 35upx;
+		padding: 10upx;
 	}
 	.infomation {
 		font-size: 15upx;
+		padding: 10upx;
 	}
 </style>
