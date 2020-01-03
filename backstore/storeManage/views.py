d9139b8ba70b286b2ed4d264735b5be0639fb288
lh <1956413161@qq.com>
2020-01-03 16:53:00 +0800

    Add total_stocks

--- backstore/storeManage/views.py
+++ backstore/storeManage/views.py
@@ -3,17 +3,23 @@ from django.db.models import Max
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from storeManage import models
+from base.models import UserNow, Area
 from base.models import Organization, Material, Department
-from base.Serializer import MaterialSerializer
+from .Serializer import TotalStockSerializer
 import json
 
 
 class TotalStockView(APIView):
 
     def get(self, request):
-        pass
-
-
-
-
-
+        user_now = UserNow.objects.get()
+        if user_now:
+            area_name = user_now.area_name
+            total_stocks = models.TotalStock.objects.filter(totalwarehouse__organization__area_name=area_name)
+            if total_stocks:
+                total_stocks_serializer = TotalStockSerializer(total_stocks, many=True)
+                return Response({"total_stocks": total_stocks_serializer.data})
+            else:
+                return Response({"message": "未查询到当地仓储信息"})
+        else:
+            return Response({"message": "用户未登录"})
