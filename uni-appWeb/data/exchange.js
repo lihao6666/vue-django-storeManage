c5e9c6522a0169089d0c1840d85b1840c657a26a
张恩普 <516380637@qq.com>
2020-01-03 14:38:33 +0800

    Finish card Ref #31

---
+++ uni-appWeb/data/exchange.js
@@ -0,0 +1,43 @@
+let data = [
+	{
+		id: 1,
+		str_iden: 'STR2001020001',
+		str_orga: '合肥库存组织01',
+		str_to: '合工大中心仓库02',
+		str_from: '合工大中心仓库01',
+		str_req_date: '2020-1-2 14:00:00',
+		str_req_department: '非中心',
+		str_remarks: '上体育课借用器材',
+		str_status: '草稿',
+		str_creator: '张恩普',
+		str_createDate: '2020-1-2 12:02:00'
+	},{
+		id: 2,
+		str_iden: 'STR2001020002',
+		str_orga: '合肥库存组织01',
+		str_to: '合工大中心仓库01',
+		str_from: '合工大中心仓库02',
+		str_date: '2020-1-2 15:00:00',
+		str_req_department: '非中心',
+		str_remarks: '上音乐课借用器材',
+		str_status: '已审批',
+		str_creator: '张恩普',
+		str_createDate: '2020-1-2 12:03:00'
+	},{
+		id: 3,
+		str_iden: 'STR2001020003',
+		str_orga: '合肥库存组织01',
+		str_to: '合工大中心仓库02',
+		str_from: '合工大中心仓库03',
+		str_date: '2020-1-2 16:00:00',
+		str_req_department: '非中心',
+		str_remarks: '上美术课借用器材',
+		str_status: '草稿',
+		str_creator: '张恩普',
+		str_createDate: '2020-1-2 12:09:00'
+	}
+]
+
+module.exports = {
+	data: data
+}
