ba8f0fc8737ea944c76ff37f1d3941175b7fbb53
张恩普 <516380637@qq.com>
2020-01-03 16:50:52 +0800

    Add my infomation page Ref #27

---
+++ uni-appWeb/data/userinfo.js
@@ -0,0 +1,19 @@
+let data = [
+	{
+		id: 1,
+		user_name: '俞浩然',
+		user_id: '123456789',
+		user_phone_number: '15198765450',
+		user_mailbox: 'haoranyu@jisuanke.com',
+		user_area: '合肥',
+		user_departments: '计蒜客中心01',
+		user_roles: '教师',
+		user_creator: '张恩普',
+		user_createDate: '2020-1-1 12:00:00',
+		user_status: '启用'
+	}
+]
+
+module.exports = {
+	data: data
+}
