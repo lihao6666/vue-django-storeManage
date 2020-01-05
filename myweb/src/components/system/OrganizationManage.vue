421b12f32906584e0a43b5f30a81e8da3c677c7b
余钱 <306594076@qq.com>
2020-01-05 18:08:27 +0800

    Organization manage interface is completed. Ref #96

--- myweb/src/components/system/OrganizationManage.vue
+++ myweb/src/components/system/OrganizationManage.vue
@@ -38,7 +38,12 @@
         <el-table-column type="index" width="50"></el-table-column>
         <el-table-column prop="dpm_name" sortable label="部门名称" :filters="dpm_orgaSet"
                          :filter-method="filter" align="center"></el-table-column>
-        <el-table-column prop="dpm_center" sortable label="是否中心" align="center"></el-table-column>
+        <el-table-column prop="dpm_center" sortable label="是否中心" :filters="dpm_centerSet"
+                         :filter-method="filter" align="center">
+          <template slot-scope="scope">
+            <el-tag :type="scope.row.dpm_center===1?'success':''">{{centortype[scope.row.dpm_center].label}}</el-tag>
+          </template>
+        </el-table-column>
         <el-table-column prop="dpm_creator" sortable label="创建人" :filters="dpm_creatorSet"
                          :filter-method="filter" align="center"></el-table-column>
         <el-table-column prop="dpm_createDate" sortable label="创建日期" align="center"></el-table-column>
@@ -96,11 +101,11 @@
                 class="el-switch-center"
                 v-model="form.dpm_center"
                 active-color="#13ce66"
-                inactive-color="#ff4949"
+                inactive-color="#0074D9"
                 active-text="中心"
-                inactive-text="其他"
-                active-value="1"
-                inactive-value="0">
+                inactive-text="非中心部门"
+                :active-value="centortype[1].value"
+                :inactive-value="centortype[0].value">
               </el-switch>
             </el-form-item>
           </el-row>
@@ -132,13 +137,14 @@
               </el-col>
               <el-switch
                 v-model="editform.dpm_center"
+                disabled
                 class="el-switch-center"
                 active-color="#13ce66"
-                inactive-color="#ff4949"
+                inactive-color="#0074D9"
                 active-text="中心"
-                inactive-text="其他"
-                active-value="1"
-                inactive-value="0">
+                inactive-text="非中心部门"
+                :active-value="centortype[1].value"
+                :inactive-value="centortype[0].value">
               </el-switch>
             </el-form-item>
           </el-row>
@@ -152,10 +158,10 @@
       </div>
       <el-row :gutter="20" class="el-row-button-save">
         <el-col :span="1" :offset="16">
-          <el-button @click="alterVisible = false">取 消</el-button>
+          <el-button @click="editVisible = false">取 消</el-button>
         </el-col>
         <el-col :span="1" :offset="3">
-          <el-button type="primary" @click="saveAlter">确 定</el-button>
+          <el-button type="primary" @click="saveEdit">确 定</el-button>
         </el-col>
       </el-row>
     </el-dialog>
@@ -163,7 +169,7 @@
 </template>
 
 <script>
-import {postAPI} from '../../api/api'
+import {getAPI, postAPI} from '../../api/api'
 export default {
   name: 'OrganizationManage',
   data () {
@@ -172,18 +178,29 @@ export default {
         pageIndex: 1,
         pageSize: 5
       },
+      centortype: [
+        {
+          value: 0,
+          label: '非中心部门'
+        },
+        {
+          value: 1,
+          label: '中心'
+        }
+      ],
       search: '',
       form: {
         dpm_name: '',
-        dpm_center: '',
+        dpm_center: 0,
         dpm_remarks: ''
       },
       dpm_name: '',
       dpm_orgaSet: [],
+      dpm_centerSet: [],
       dpm_creatorSet: [],
       editform: {
         dpm_name: '',
-        dpm_center: '',
+        dpm_center: 0,
         dpm_remarks: ''
       },
       tableData: [],
@@ -200,16 +217,26 @@ export default {
   methods: {
     getData () {
       let _this = this
-      postAPI('/base/departments').then(function (res) {
+      getAPI('/base/departments').then(function (res) {
+        console.log(res.data)
         _this.tableData = res.data.departments
         _this.dpm_orgaSet = []
+        _this.dpm_centerSet = []
         _this.dpm_creatorSet = []
         _this.find()
         let orgaset = new Set()
+        let centerset = new Set()
         let creatorset = new Set()
         for (let i in _this.tableData) {
           orgaset.add(_this.tableData[i]['dpm_name'])
           creatorset.add(_this.tableData[i]['dpm_creator'])
+          centerset.add(_this.tableData[i]['dpm_center'])
+        }
+        for (let i of centerset) {
+          _this.dpm_centerSet.push({
+            text: _this.centortype[i].label,
+            value: i
+          })
         }
         for (let i of orgaset) {
           _this.dpm_orgaSet.push({
@@ -247,7 +274,7 @@ export default {
     // 一键清除新增表单
     clearform () {
       this.form.dpm_name = ''
-      this.form.dpm_center = ''
+      this.form.dpm_center = 0
       this.form.dpm_remarks = ''
     },
     // 新增
@@ -256,11 +283,34 @@ export default {
     },
     // 停用操作
     handleStop (row) {
-      postAPI('/base/departments', {data: row, status: 0}).then(function (res) {
-        console.log(res)
-      }).catch(function (err) {
-        console.log(err)
+      this.$confirm('确定要停用吗？', '提示', {
+        type: 'warning'
       })
+        .then(() => {
+          row.dpm_status = 0
+          console.log(row)
+          let _this = this
+          postAPI('/base/departmentStatus', row).then(function (res) {
+            console.log(res.data)
+            if (res.data.signal === 0) {
+              _this.$message.success(`停用成功`)
+              _this.getData()
+            } else {
+              _this.$message.error(res.data.message)
+              row.dpm_status = 1
+            }
+          }).catch(function (err) {
+            console.log(err)
+            _this.$message.error(`停用失败`)
+            row.dpm_status = 1
+          })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消停用'
+          })
+        })
     },
     // 查询
     find () {
@@ -274,39 +324,81 @@ export default {
     },
     // 启用
     handleStart (row) {
-      postAPI('/base/departments', {data: row, status: 1}).then(function (res) {
-        console.log(res)
-      }).catch(function (err) {
-        console.log(err)
+      this.$confirm('确定要启用吗？', '提示', {
+        type: 'warning'
       })
+        .then(() => {
+          row.dpm_status = 1
+          let _this = this
+          postAPI('/base/departmentStatus', row).then(function (res) {
+            if (res.data.signal === 0) {
+              _this.$message.success(`启用成功`)
+              _this.getData()
+            } else {
+              _this.$message.error(res.data.message)
+              row.dpm_status = 0
+            }
+          }).catch(function (err) {
+            console.log(err)
+            _this.$message.error(`启用失败`)
+            row.dpm_status = 0
+          })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消启用'
+          })
+        })
     },
     // 编辑操作
     handleEdit (row) {
       this.editform.dpm_name = row.dpm_name
       this.editform.dpm_center = row.dpm_center
       this.editform.dpm_remarks = row.dpm_remarks
-      this.dpm_name = row.dpm_name
+      this.editform.id = row.id
       this.editVisible = true
     },
     // 保存编辑
     saveEdit () {
-      this.editVisible = false
-      this.$message.success(`修改成功`)
-      postAPI('/base/departments', {data: this.editform, dpm_name: this.dpm_name}).then(function (res) {
-        console.log(res)
+      console.log(this.editform)
+      if (!this.editform.dpm_name) {
+        this.$message.error(`请填写完信息`)
+        return
+      }
+      let _this = this
+      postAPI('/base/departmentUpdate', this.editform).then(function (res) {
+        if (res.data.signal === 0) {
+          _this.$message.success(`修改成功`)
+          _this.editVisible = false
+          _this.getData()
+        } else {
+          _this.$message.error(res.data.message)
+        }
       }).catch(function (err) {
         console.log(err)
+        _this.$message.error(`修改失败`)
       })
     },
     // 保存新增
     saveAlter () {
-      this.alterVisible = false
-      this.$message.success(`新增成功`)
-      this.clearform()
-      postAPI('/base/departments', {data: this.form, table: 'department'}).then(function (res) {
-        console.log(res)
+      if (!this.form.dpm_name) {
+        this.$message.error(`请填写完信息`)
+        return
+      }
+      let _this = this
+      postAPI('/base/departmentAdd', this.form).then(function (res) {
+        if (res.data.signal === 0) {
+          _this.$message.success(`新增成功`)
+          _this.alterVisible = false
+          _this.clearform()
+          _this.getData()
+        } else {
+          _this.$message.error(res.data.message)
+        }
       }).catch(function (err) {
         console.log(err)
+        _this.$message.error(`新增失败`)
       })
     },
     // 分页导航
@@ -367,5 +459,8 @@ export default {
   }
   .green {
     color: GREEN;
+  }
+  .l {
+    color: #0074D9;
   }
 </style>
