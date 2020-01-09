b7b6163fd2ed31caef217f6360033fb19dd73294
zhongxiaolin <2716371951@qq.com>
2020-01-09 16:49:58 +0800

    Update the page of store Ref #127

--- uni-appWeb/pages/detail/detail.vue
+++ uni-appWeb/pages/detail/detail.vue
@@ -1,62 +1,128 @@
 <template>
 	<view class="content">
+<!-- 		<view v-for="item in detailList" :key="item.id" class="card-set"> -->
+		
+		<view class="material">
+			<text class="material-title">物料编号</text>
+			<text class="material-input">
+				{{ material_iden }}
+			</text>
+		</view>
 		<view class="material">
 			<text class="material-title">物料名称</text>
 			<text class="material-input">
-				1
+				{{ material_name }}
 			</text>
 		</view>
 		<view class="material">
-			<text class="material-title">物料编码</text>
+			<text class="material-title">仓库编号</text>
 			<text class="material-input">
-				2
+				{{ total_iden }}
 			</text>
 		</view>
 		<view class="material">
-			<text class="material-title">规格</text>
+			<text class="material-title">仓库名称</text>
 			<text class="material-input">
-				3
+				{{ total_name }}
 			</text>
 		</view>
 		<view class="material">
-			<text class="material-title">型号</text>
+			<text class="material-title">库存组织编号</text>
 			<text class="material-input">
-				4
+				{{ orga_iden }}
 			</text>
 		</view>
 		<view class="material">
-			<text class="material-title">计量单位</text>
+			<text class="material-title">库存组织名称</text>
 			<text class="material-input">
-				5
+				{{ orga_name }}
 			</text>
 		</view>
 		<view class="material">
-			<text class="material-title">存货属性</text>
+			<text class="material-title">物料规格</text>
 			<text class="material-input">
-				6
+				{{ material_specification }}
 			</text>
 		</view>
 		<view class="material">
-			<text class="material-title">物料状态</text>
+			<text class="material-title">物料类型</text>
 			<text class="material-input">
-				7
+				{{ material_model }}
 			</text>
 		</view>
-		<view class="address-add-btn" @tap="apply">前往申领</view>
-    </view>
+		<view class="material">
+			<text class="material-title">物料单位</text>
+			<text class="material-input">
+				{{ material_meterage }}
+			</text>
+		</view>
+		<view class="material">
+			<text class="material-title">现存量</text>
+			<text class="material-input">
+				{{ ts_present_num }}
+			</text>
+		</view>
+		<view class="material">
+			<text class="material-title">现存单价</text>
+			<text class="material-input">
+				{{ ts_present_price }}
+			</text>
+		</view>
+		<view class="material">
+			<text class="material-title">现存总价</text>
+			<text class="material-input">
+				{{ ts_present_sum }}
+			</text>
+		</view>		
+		<view class="address-add-btn" @click="apply">前往申领</view>
+	</view>
 </template>
 
 <script>
+	// import detailData from '../../data/English.js'
+	var _this;
 export default {
+	data() {
+		return {
+			material_iden: '',
+			material_name: '',
+			total_iden: '',
+			total_name: '',
+			orga_iden: '',
+			orga_name: '',
+			material_specification: '',
+			material_model: '',
+			material_meterage: '',
+			ts_present_num: '',
+			ts_present_price: '',
+			ts_present_sum: ''
+		}
+	},
+	mounted() {
+		_this = this
+	},
 	methods: {
 		apply:function(e){
-			uni.navigateTo({
-				url: '../reg/reg',
-				success: res => {},
-				fail: () => {},
-				complete: () => {}
+			uni.removeStorageSync('details')
+			uni.switchTab({
+				url: '../index/index',
 			});	
-		}
+		},
+	},
+	onLoad: function() {
+		var  item= uni.getStorageSync('details')
+		this.material_iden = item.material_iden
+		this.material_name = item.material_name
+		this.total_iden = item.total_iden
+		this.total_name = item.total_name
+		this.orga_iden = item.orga_iden
+		this.orga_name = item.orga_name
+		this.material_specification = item.material_specification
+		this.material_model = item.material_model
+		this.material_meterage = item.material_meterage
+		this.ts_present_num = item.ts_present_num
+		this.ts_present_price = item.ts_present_price
+		this.ts_present_sum = item.ts_present_sum
 	},
 }
 </script>
@@ -70,7 +136,7 @@ export default {
 		align-items: center;
 		position: relative;
 		padding: 0 30upx;
-		height: 100upx;
+		height: 92upx;
 		background: #fff;
 		border-bottom:1upx solid #F8F8F8;
 	
