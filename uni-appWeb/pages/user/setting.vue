3b69ee4af207d28c8c1970afd6abed061b339cdd
张恩普 <516380637@qq.com>
2020-01-09 14:32:45 +0800

    Interface in purchase detail page has realized Ref #134

--- uni-appWeb/pages/user/setting.vue
+++ uni-appWeb/pages/user/setting.vue
@@ -29,9 +29,7 @@
 				    success: function (choose) {
 				        if (choose.confirm) {
 				            _this.$http.post('/base/loginExit', {user_now_iden}).then(([err,res]) => {
-								uni.removeStorageSync('user_info')
-								uni.removeStorageSync('user_now_iden')
-								uni.removeStorageSync('power')
+								uni.clearStorageSync()
 				            	uni.reLaunch({
 				            	    url: '../login/login',
 				            	});
